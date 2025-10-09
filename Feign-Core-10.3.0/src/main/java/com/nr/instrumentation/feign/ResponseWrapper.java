656488767856fe25c8e24c08a68e1dc2e5252933
Doug Hilpipre <dhilpipre@newrelic.com>
2025-10-09 09:41:42 -0500

    updates to use updated Agent API calls

--- Feign-Core-10.3.0/src/main/java/com/nr/instrumentation/feign/ResponseWrapper.java
+++ Feign-Core-10.3.0/src/main/java/com/nr/instrumentation/feign/ResponseWrapper.java
@@ -1,17 +1,21 @@
 package com.nr.instrumentation.feign;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 import com.newrelic.api.agent.HeaderType;
+import com.newrelic.api.agent.Headers;
 import com.newrelic.api.agent.InboundHeaders;
 
 import feign.Response;
 
-public class ResponseWrapper implements InboundHeaders {
-	
+public class ResponseWrapper implements Headers {
+
 	private Response response = null;
-	
+
 	public ResponseWrapper(Response resp) {
 		response = resp;
 	}
@@ -24,13 +28,43 @@ public class ResponseWrapper implements InboundHeaders {
 	@Override
 	public String getHeader(String name) {
 		Collection<String> values =response.headers().get(name);
-		
+
 		if(values != null && !values.isEmpty()) {
 			Iterator<String> iterator = values.iterator();
 			return iterator.next();
 		}
-		
+
 		return null;
+	}
+
+	@Override
+	public Collection<String> getHeaders(String name) {
+		return response.headers().get(name);
+	}
+
+	@Override
+	public void setHeader(String name, String value) {
+		addHeader(name, value);
+	}
+
+	@Override
+	public void addHeader(String name, String value) {
+		List<String> list = new ArrayList<String>();
+		Collection<String> existingValues = response.headers().get(name);
+		if(existingValues != null && !existingValues.isEmpty()) {
+			list.addAll(existingValues);
+		}
+		response.headers().put(name, list);
+	}
+
+	@Override
+	public Collection<String> getHeaderNames() {
+		return response.headers().keySet();
+	}
+
+	@Override
+	public boolean containsHeader(String name) {
+		return getHeaderNames().contains(name);
 	}
 
 }
--- Feign-Core/src/main/java/com/nr/instrumentation/feign/ResponseWrapper.java
+++ Feign-Core/src/main/java/com/nr/instrumentation/feign/ResponseWrapper.java
@@ -1,14 +1,18 @@
 package com.nr.instrumentation.feign;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 import com.newrelic.api.agent.HeaderType;
+import com.newrelic.api.agent.Headers;
 import com.newrelic.api.agent.InboundHeaders;
 
 import feign.Response;
 
-public class ResponseWrapper implements InboundHeaders {
+public class ResponseWrapper implements Headers {
 	
 	private Response response = null;
 	
@@ -31,6 +35,36 @@ public class ResponseWrapper implements InboundHeaders {
 		}
 		
 		return null;
+	}
+
+	@Override
+	public Collection<String> getHeaders(String name) {
+		return response.headers().get(name);
+	}
+
+	@Override
+	public void setHeader(String name, String value) {
+		addHeader(name, value);
+	}
+
+	@Override
+	public void addHeader(String name, String value) {
+		List<String> list = new ArrayList<String>();
+		Collection<String> existingValues = response.headers().get(name);
+		if(existingValues != null && !existingValues.isEmpty()) {
+			list.addAll(existingValues);
+		}
+		response.headers().put(name, list);
+	}
+
+	@Override
+	public Collection<String> getHeaderNames() {
+		return response.headers().keySet();
+	}
+
+	@Override
+	public boolean containsHeader(String name) {
+		return getHeaderNames().contains(name);
 	}
 
 }
